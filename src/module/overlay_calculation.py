b5628e8365dd6a7dee4fd62a28ad31e736ea0a57
Choi <chungkang.choi@dornier-group.com>
2024-01-16 16:06:52 +0100

    overlay logic done

--- src/module/overlay_calculation.py
+++ src/module/overlay_calculation.py
@@ -1,37 +1,50 @@
 import pandas as pd
 import rasterio
 import numpy as np
+import copy
 
+
+# set input file path
 directory_path = r'data\\step1_input_data\\south_area\\'
 input_excel_path = directory_path + r'MCA_criteria_template.xlsx'
 
+
+
+# create panda data frame for each purpose
 df_input_excel = pd.read_excel(input_excel_path)
 
 # Filter DataFrame for AOI, Include, and Exclude
 # AOI dafa frame
 df_AOI = df_input_excel[df_input_excel['AOI'] == 1]
+AOI_file_path = directory_path + df_AOI.iloc[0]['file_name']
 
 # Filter rows where are AOI, include, exclude
 scored_df = df_input_excel[(df_input_excel['AOI'] == 1) | (df_input_excel['include'] != 1) | (df_input_excel['exclude'] != 1)]
 
-# Calculate the weight of layers
-# Calculate the sum of layer_weight for filtered rows
-total_layer_weight = scored_df['layer_weight'].sum()
 
-# Add a new column 'layer_weight_calculation' with the calculated values
-df_input_excel['layer_weight_rate'] = (df_input_excel['layer_weight'] / total_layer_weight).round(3)
-print(df_input_excel)
+
+# Calculate 'layer_weight_rate' column
+df_input_excel['layer_weight_rate'] = (df_input_excel['layer_weight'] / scored_df['layer_weight'].sum()).round(3)
+
+
 
 # x, y resolution 과 extend 가 같은지 확인해서 안되면 에러메시지
 
-with rasterio.open(directory_path + df_AOI.iloc[0]['file_name']) as AOI_dataset:
-    AOI_raster_value = AOI_dataset.read(1).astype(float)
+
+
+# create base raster array for raster calculation, should be 0 value in AOI / using for raster cell calculation
+with rasterio.open(AOI_file_path) as AOI_dataset:
+    array_calculation = np.array(copy.deepcopy(AOI_dataset.read(1).astype(float)))-1
+
+
 
 # filtering scored layers: layer_weight is not null
 df_scored_layers = df_input_excel[df_input_excel['layer_weight'].notna()]
 
-# filtering non-scored layers: layer_weight is null (AHOI, include, exclude)
+# filtering non-scored layers: layer_weight is null (AOI, include, exclude)
 df_nonscored_layers = df_input_excel[df_input_excel['layer_weight'].isna()]
+
+
 
 # calculate layer weight
 # Iterate over each row in the DataFrame
@@ -39,14 +52,11 @@ for index, row in df_scored_layers.iterrows():
     # Access values in each row
     file_path = directory_path + row['file_name']
 
-    print(row['file_name'] + ' : ')
     with rasterio.open(file_path) as dataset:
         dataset_raster_value = dataset.read(1)
-        print(dataset_raster_value * row['layer_weight_rate'])
-        AOI_raster_value +=  dataset_raster_value * row['layer_weight_rate']
+        array_calculation +=  dataset_raster_value * row['layer_weight_rate']
 
-    print('result_MCA in progress:')
-    print(AOI_raster_value)
+
 
 # calculate AOI, Include, Exclude
 # Iterate over each row in the DataFrame
@@ -56,28 +66,21 @@ for index, row in df_nonscored_layers.iterrows():
     include = row['include']
     exclude = row['exclude']
  
-    print(row['file_name'] + ' : ')
     if include == 1:  # multiply
         with rasterio.open(file_path) as dataset:
             dataset_raster_value = dataset.read(1)
-            print(dataset_raster_value)
-            AOI_raster_value *= dataset_raster_value
+            array_calculation *= dataset_raster_value
             
     elif exclude == 1:  # 0,1 inverted multiply
         with rasterio.open(file_path) as dataset:
             inverted_dataset_raster_value = 1 - dataset.read(1)
-            print(inverted_dataset_raster_value)
-            AOI_raster_value *= inverted_dataset_raster_value
+            array_calculation *= inverted_dataset_raster_value
 
-    print('result_MCA in progress:')
-    print(AOI_raster_value)
 
-print('result_MCA final:')
-print(AOI_raster_value)
 
 # smaller than 1 means NoData
 no_data_value = 0
-AOI_raster_value[AOI_raster_value < 1] = no_data_value
+array_calculation[array_calculation < 1] = no_data_value
 
 with rasterio.open(AOI_file_path) as AOI_dataset:
     profile = AOI_dataset.profile
@@ -85,4 +88,6 @@ with rasterio.open(AOI_file_path) as AOI_dataset:
     profile['compress'] = 'DEFLATE' # same as high compression(default) in QGIS compression option
 
 with rasterio.open(directory_path + r'MCA_result.tif', 'w', **profile) as dst:
-    dst.write(AOI_raster_value.astype(profile['dtype']), 1)
+    dst.write(array_calculation.astype(profile['dtype']), 1)
+
+
