67a21fefd4886dde2b3c70e7b45f3e307e22ae54
Choi <chungkang.choi@dornier-group.com>
2024-01-29 12:26:09 +0100

    modify step3

--- src/module/step3_convert_CRS.py
+++ src/module/step3_convert_CRS.py
@@ -3,12 +3,12 @@ import pandas as pd
 import geopandas as gpd
 import rasterio
 from rasterio.warp import calculate_default_transform, reproject, Resampling
-from pyproj import Transformer
+import shutil
 
-path_data = r'data\\step1\\'
-path_read = r'data\\step2\\'
-path_write = r'data\\step3\\'
-input_excel_path = path_read + r'step2_excel_template.xlsx'
+input_data_path = r'data\\step1\\'
+input_excel_path = r'data\\step2\\'
+output_path = r'data\\step3\\'
+input_excel_path = input_excel_path + r'step2_excel_template.xlsx'
 
 # create panda data frame for each purpose
 df_input_excel = pd.read_excel(input_excel_path)
@@ -17,24 +17,31 @@ df_input_excel = pd.read_excel(input_excel_path)
 shp_geojson_df = df_input_excel[df_input_excel['file_name'].str.lower().str.endswith(('.shp', '.geojson'))]
 
 for idx, row in shp_geojson_df.iterrows():
+    gdf = gpd.read_file(os.path.join(input_data_path, row['file_name']))  # add path
+
+    # convert CRS
     if row['source_CRS'] != row['target_CRS']:
-        gdf = gpd.read_file(os.path.join(path_data, row['file_name']))  # add path
-        
-        # convert CRS to EPSG:3857
         gdf = gdf.to_crs(row['target_CRS'])
-        
-        output_file_name = os.path.splitext(row['file_name'])[0] + '_EPSG3857.shp'  # change file name
-        output_file_path = os.path.join(path_write, output_file_name)  # add path
+        output_file_name = os.path.splitext(row['file_name'])[0] + '_CRS.shp'  # change file name
+        output_file_path = os.path.join(output_path, output_file_name)  # add path
         gdf.to_file(output_file_path, driver='ESRI Shapefile')  # save as Shapefile
+
+    # only rename and save as shape file
+    else:
+        output_file_name = os.path.splitext(row['file_name'])[0] + '_CRS.shp'  # change file name
+        output_file_path = os.path.join(output_path, output_file_name)  # add path
+        gdf.to_file(output_file_path, driver='ESRI Shapefile')  # save as Shapefile
+
 
 # process '.tif' file
 tif_df = df_input_excel[df_input_excel['file_name'].str.lower().str.endswith('.tif')]
 
 for idx, row in tif_df.iterrows():
+    input_file_path = os.path.join(input_data_path, row['file_name'])  # add path
+    output_file_name = os.path.splitext(row['file_name'])[0] + '_CRS.tif'  # change file name
+    output_file_path = os.path.join(output_path, output_file_name)  # add path
+    
     if row['source_CRS'] != row['target_CRS']:
-        input_file_path = os.path.join(path_data, row['file_name'])  # add path
-        output_file_name = os.path.splitext(row['file_name'])[0] + '_EPSG3857.tif'  # change file name
-        output_file_path = os.path.join(path_write, output_file_name)  # add path
         with rasterio.open(input_file_path) as src:
             transform, width, height = calculate_default_transform(src.crs, row['target_CRS'], src.width, src.height, *src.bounds)
             kwargs = src.meta.copy()
@@ -50,3 +57,5 @@ for idx, row in tif_df.iterrows():
                         dst_transform=transform,
                         dst_crs=row['target_CRS'],
                         resampling=Resampling.nearest)
+    else:
+        shutil.copy(input_file_path, output_file_path)
