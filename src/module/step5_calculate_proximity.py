bc0a46c516a6b54fce4cd7a66a3f8df70f43daf7
Choi <chungkang.choi@dornier-group.com>
2024-01-31 12:38:41 +0100

    step5 logic

--- src/module/step5_calculate_proximity.py
+++ src/module/step5_calculate_proximity.py
@@ -7,8 +7,8 @@ Description: Calculate Proximity
 
 import os
 import pandas as pd
-import rasterio
 from osgeo import gdal
+import shutil  # library for copying files
 
 def calculate_proximity(input_raster_path, output_raster_path, max_distance):
     # Open the source raster file
@@ -17,16 +17,31 @@ def calculate_proximity(input_raster_path, output_raster_path, max_distance):
         print(f"Unable to open {input_raster_path} for reading")
         return
 
-    # Create the output raster file
+    # Define GDAL creation options for high compression
+    creation_options = [
+        'TILED=YES',
+        'COMPRESS=DEFLATE',  # Use DEFLATE compression
+        'PREDICTOR=1',       # Use predictor 1 (suitable for integer data)
+        'ZLEVEL=9'           # Set the compression level (1=fastest, 9=best compression)
+    ]
+
+    # Create the output raster file with compression options
     driver = gdal.GetDriverByName('GTiff')
-    out_ds = driver.CreateCopy(output_raster_path, src_ds, 0)
+    out_ds = driver.Create(output_raster_path, src_ds.RasterXSize, src_ds.RasterYSize,1, gdal.GDT_Float32, options=creation_options)
     if out_ds is None:
         print(f"Unable to create {output_raster_path}")
         return
 
+    out_ds.SetGeoTransform(src_ds.GetGeoTransform())
+    out_ds.SetProjection(src_ds.GetProjectionRef())
+
+    # Initialize band to 0 (or another suitable no data value)
+    out_band = out_ds.GetRasterBand(1)
+    out_band.Fill(0)  # or another no data value
+
     # Calculate proximity
-    options = ['MAXDIST={}'.format(max_distance), 'VALUES=1']
-    gdal.ComputeProximity(src_ds.GetRasterBand(1), out_ds.GetRasterBand(1), options)
+    options = ['MAXDIST={}'.format(max_distance), 'VALUES=1', 'DISTUNITS=GEO']
+    gdal.ComputeProximity(src_ds.GetRasterBand(1), out_band, options)
 
     # Clean up
     src_ds = None
@@ -49,24 +64,26 @@ tif_df = df_input_excel[df_input_excel['file_name'].str.lower().str.endswith('.t
 # Process '.tif' files for proximity calculation
 for idx, row in tif_df.iterrows():
     input_file_path = os.path.join(input_path, row['file_name'])
+  
     if row['proximity'] == 1:
         output_file_name = os.path.splitext(row['file_name'])[0] + '_proximity.tif'
         output_file_path = os.path.join(output_path, output_file_name)
         calculate_proximity(input_file_path, output_file_path, 50000)  # 50 km in meters
-
-        # Add file details to the processed_files list
-        processed_files.append({
-            'file_name': output_file_name,
-            'source_resolution(m)': row['source_resolution(m)'],
-            'target_resolution(m)': row['target_resolution(m)'],
-            'source_CRS': row['source_CRS'],
-            'target_CRS': row['target_CRS'],
-            'AOI': row['AOI'],
-            'proximity': '1'
-        })
+   
     else:
-        # For files not requiring proximity calculation, simply copy the existing details
-        processed_files.append(row.to_dict())
+        output_file_name = row['file_name']
+        output_file_path = os.path.join(output_path, output_file_name)
+        shutil.copy(input_file_path, output_file_path)
+
+    # add file info to excel
+    processed_files.append({
+        'file_name': output_file_name,
+        'source_resolution(m)': row['source_resolution(m)'],
+        'target_resolution(m)': row['target_resolution(m)'],
+        'source_CRS': row['source_CRS'],
+        'target_CRS': row['target_CRS'],
+        'AOI': row['AOI'],
+    })
 
 # Create a DataFrame from the processed_files list
 df_processed = pd.DataFrame(processed_files)
