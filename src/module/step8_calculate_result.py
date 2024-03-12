0811c6d17b1dbe836ae187e79099651dc568f869
Choi <chungkang.choi@dornier-group.com>
2024-03-12 14:11:17 +0100

    MCA result

--- src/module/step8_calculate_result.py
+++ src/module/step8_calculate_result.py
@@ -35,19 +35,27 @@ def process_result_calculation(input_path, output_path, input_excel_path):
     # filtering non-scored layers: layer_weight is null (AOI, exclusion)
     df_nonscored_layers = df_input_excel[df_input_excel['layer_weight_rate'].isna()]
 
-    print("scored------------------------------------------------")
-    print(df_scored_layers)
-    print("nonscored------------------------------------------------")
-    print(df_nonscored_layers)
-
     # calculate layer weight
     for index, row in df_scored_layers.iterrows():
-        # Access values in each row
         file_path = input_path + row['file_name']
+       
+        try:
+            with rasterio.open(file_path) as dataset:
+                dataset_raster_value = dataset.read(1)
+                # NoData 값을 확인하고 0으로 설정
+                if dataset.nodata is not None:
+                    dataset_raster_value[dataset_raster_value == dataset.nodata] = 0
 
-        with rasterio.open(file_path) as dataset:
-            dataset_raster_value = dataset.read(1)
-            array_calculation += dataset_raster_value * row['layer_weight_rate']
+                # 래스터 데이터 타입이 기대하는 타입인지 확인
+                if not np.issubdtype(dataset_raster_value.dtype, np.number):
+                    print(f"Error: Raster data type is not numeric at path {file_path}")
+                    continue
+
+                # 배열 연산 수행
+                array_calculation += dataset_raster_value * row['layer_weight_rate']
+
+        except rasterio.errors.RasterioIOError as e:
+            print(f"Error reading raster file at path {file_path}: {e}")
 
     # calculate AOI, Exclusion
     for index, row in df_nonscored_layers.iterrows():
@@ -79,9 +87,8 @@ def process_result_calculation(input_path, output_path, input_excel_path):
 
     with rasterio.open(AOI_file_path) as AOI_dataset:
         profile = AOI_dataset.profile
-        profile['nodata'] = no_data_value
-        profile['compress'] = 'DEFLATE' # same as high compression(default) in QGIS compression option
-
+        profile.update(dtype=rasterio.float32, nodata=no_data_value, compress='DEFLATE')
+    
     with rasterio.open(output_path + r'MCA_result.tif', 'w', **profile) as dst:
         dst.write(array_calculation.astype(profile['dtype']), 1)
 
